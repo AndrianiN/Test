163f5f7e62b9c6b4217dab8d0da6c25aedcafcf0
kostas-rigan <t8200145@aueb.gr>
2021-10-28 11:33:15 +0100

    Adding java files to test repository

---
+++ TestJavaFile.java
@@ -0,0 +1,10 @@
+public class TestJavaFile {
+	public static void main(String[] args) {
+		greetAndriani();
+	}
+
+	public static void greetAndriani() {
+		System.out.println("Hi Andriani");
+	}
+}
+
